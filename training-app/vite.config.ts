1e11b93381fd3df1227b7a288442f440e5629d8a
Jibril1001 <jibrilmustefa9283@gmail.com>
2026-03-05 21:57:32 +0300

    env change for vite config of frontend fix 2.

--- training-app/vite.config.ts
+++ training-app/vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const frontendUrl = process.env.FRONTEND_URL ?? 'localhost';
+const frontendHost = process.env.FRONTEND_URL?.replace(/^https?:\/\//, '') ?? 'localhost';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: Number(process.env.VITE_PORT),
-    allowedHosts: [frontendUrl],
+    port: Number(process.env.VITE_PORT) || 5173,
+    allowedHosts: [frontendHost],
   },
 })
