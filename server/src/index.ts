bf5fd872d2329e588db926b9c65f20e60c07051e
Jibril1001 <jibrilmustefa9283@gmail.com>
2026-03-05 20:51:05 +0300

    render deploy yaml file added

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ async function startServer() {
   app.use(express.json());
   app.use(cookieParser());
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }));
 
